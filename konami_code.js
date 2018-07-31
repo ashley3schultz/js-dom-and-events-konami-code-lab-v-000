97ec872f88fc9c93c9c6525014f450729f56e655
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-31 03:57:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,7 +9,7 @@ function init() {
     if (key === code[index]) {
       index++
       if (index === code.length) {
-        alert("Hurray!")
+        window.alert("Hurray!")
         index = 0
       }
     } else {
